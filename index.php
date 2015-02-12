38cbe8935e2d9bf42bf06ab23ba53d320059dc09
Errietta Kostala <errietta@errietta.me>
2015-02-12 12:51:03 +0000

    Add youtube video underneath form

--- index.php
+++ index.php
@@ -619,6 +619,8 @@ System coordinator</p>
             </fieldset>
             </form>
             <br />
+
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/2ZzlNMUmadM" frameborder="0" allowfullscreen></iframe>
 		</div>
         
       	<div class="col-6 col-sm-4 col-lg-4">
