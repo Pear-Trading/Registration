041cc84467376a1c24b31f0924efc7009a93e1b3
Errietta Kostala <errietta@errietta.me>
2015-02-12 13:08:55 +0000

    Add local customer text

--- config/am_i_local.php
+++ config/am_i_local.php
@@ -1,3 +1,6 @@
 <h1>Local Business?</h1>
 <p>We have defined the term 'local' to mean any business owned and operated within the Lancaster City Council boundaries where the owner also lives within the Lancaster City Council boundaries.</p>
 <p>If this is you, then please sign up to the become a PEAR TRADING business.</p>
+<h1>Local Customers</h1>
+<p>Feel free to sign up and spread the word with your friends and businesses you shop with.</p>
+<p><a href="http://www.peartrade.org" target="_blank">More information</a></p>
