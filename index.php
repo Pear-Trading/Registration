2c2c2c1f0b77f09753ed18e219965e4d018baf19
Errietta Kostala <errietta@errietta.me>
2015-02-17 09:29:56 +0000

    Fix intendation

--- index.php
+++ index.php
@@ -29,164 +29,151 @@ require './config/utils.php';
 			var yearOfBirthPicker = $('#dpYears').datepicker().on('changeDate', function(ev) {$('.datepicker').hide();});
 		});
 	</script>
-    
-	<script>
-		$(function(){
-			    
-			$('#trader_details').hide();
-			$('#customer_details').show();
-			$('#account_name').text('Please enter your full name');
-			$('#name').attr('placeholder',"full name");
-			$('#account_business_name').prop("required", false);
-			$('#b_postcode').prop("required", false);
-						
-			$("[name='account']").click(function(){
-				if ($(this).attr("value") == "customer")
-				{
-					$('#trader_details').hide();
-					$('#customer_details').show();
-					$('#account_name').text('Please enter your full name');
-					$('#name').attr('placeholder',"full name");
-					$('#account_business_name').prop("required", false);
-					$('#b_postcode').prop("required", false);
-				} 
-				else if (($(this).attr("value") == "business") || ($(this).attr("value") == "organisation"))
-				{
-					$('#trader_details').show();
-					$('#customer_details').hide();
-					$('#account_business_name').prop("required", true);
-					$('#b_postcode').prop("required", true);
-				}
-			});
-			
-		 	$("input,select,textarea").not("[type=submit]").jqBootstrapValidation({
-				
-				preventSubmit: true,
-					submitError: function($form, event, errors) {
-						
-						console.log("submitError");
-						alert("There seems to be an error submitting your details, please check you have filled in all the required fields.");
-					},
-					
-				submitSuccess: function ($form, event) {
-					
-					console.log("submitSuccess");
-					var service;
-					var manufacturer;
-					var retailer;
-					var type_fixed;
-					var type_normadic;
-					var wholesaler;
-					/*var environmental;
-					var social;
-					var wellbeing;
-					var economic;*/
-					
-					if ($('#service').is(':checked')) {
-						service = "&service=1";
-					} else {
-						service = "&service=0";
-					}
-					
-					if ($('#manufacturer').is(':checked')) {
-						manufacturer = "&manufacturer=1";
-					} else {
-						manufacturer = "&manufacturer=0";
-					}
-					
-					if ($('#wholesaler').is(':checked')) {
-						wholesaler = "&wholesaler=1";
-					} else {
-						wholesaler = "&wholesaler=0";
-					}
-					
-					if ($('#retailer').is(':checked')) {
-						retailer = "&retailer=1";
-					} else {
-						retailer = "&retailer=0";
-					}
-					
-					if ($('#fixed').is(':checked')) {
-						type_fixed = "&fixed=1";
-					} else {
-						type_fixed = "&fixed=0";
-					}
-					if ($('#normadic').is(':checked')) {
-						type_normadic = "&normadic=1";
-					} else {
-						type_normadic = "&normadic=0";
-					}
-					
-					/*if ($('#environmental').is(':checked')) {
-						environmental = "&environmental=environmental";
-					} else {
-						environmental = "&environmental=";
-					}
-					
-					if ($('#social').is(':checked')) {
-						social = "&social=social";
-					} else {
-						social = "&social=";
-					}
-					
-					if ($('#economic').is(':checked')) {
-						economic = "&economic=economic";
-					} else {
-						economic = "&economic=";
-					}
-					
-					if ($('#wellbeing').is(':checked')) {
-						wellbeing = "&wellbeing=wellbeing";
-					} else {
-						wellbeing = "&wellbeing=";
-					}*/
-					
-					
-					var dob = "&age_group="+$('#age_group').val();
 
-					var form = $('#add_user_form').find('input, textarea, select')
-                       .not(':checkbox')
-                       .serialize()
+    <script>
+    $(function(){
+        $('#trader_details').hide();
+        $('#customer_details').show();
+        $('#account_name').text('Please enter your full name');
+        $('#name').attr('placeholder',"full name");
+        $('#account_business_name').prop("required", false);
+        $('#b_postcode').prop("required", false);
 
-					
-					//form += environmental + social + wellbeing + economic + service + manufacturer + wholesaler+ retailer + type_fixed + type_normadic + dob;
-					
-					form += service + manufacturer + wholesaler+ retailer + type_fixed + type_normadic + dob;
-					
-					console.log(form);   
-					
-				 $.ajax({
-					type: 'POST',
-					url: $form.attr('action'),
-					data: form,
-					success: function(data)
-					{ 
-						// just to confirm ajax submission
-						console.log('submitted successfully!');
-						var received_data = JSON.parse(data);
-						alert(received_data.message);
-						
-						if(received_data.response == true){
-							//alert(received_data.message);
+        $("[name='account']").click(function(){
+            if ($(this).attr("value") == "customer")
+            {
+                $('#trader_details').hide();
+                $('#customer_details').show();
+                $('#account_name').text('Please enter your full name');
+                $('#name').attr('placeholder',"full name");
+                $('#account_business_name').prop("required", false);
+                $('#b_postcode').prop("required", false);
+            } 
+            else if (($(this).attr("value") == "business") || ($(this).attr("value") == "organisation"))
+            {
+                $('#trader_details').show();
+                $('#customer_details').hide();
+                $('#account_business_name').prop("required", true);
+                $('#b_postcode').prop("required", true);
+            }
+        });
+
+        $("input,select,textarea").not("[type=submit]").jqBootstrapValidation({
+            preventSubmit: true,
+            submitError: function($form, event, errors) {
+                console.log("submitError");
+                alert("There seems to be an error submitting your details, please check you have filled in all the required fields.");
+            },
+            submitSuccess: function ($form, event) {
+                console.log("submitSuccess");
+                var service;
+                var manufacturer;
+                var retailer;
+                var type_fixed;
+                var type_normadic;
+                var wholesaler;
+                /*var environmental;
+                var social;
+                var wellbeing;
+                var economic;*/
+
+                if ($('#service').is(':checked')) {
+                    service = "&service=1";
+                } else {
+                    service = "&service=0";
+                }
+
+                if ($('#manufacturer').is(':checked')) {
+                    manufacturer = "&manufacturer=1";
+                } else {
+                    manufacturer = "&manufacturer=0";
+                }
+
+                if ($('#wholesaler').is(':checked')) {
+                    wholesaler = "&wholesaler=1";
+                } else {
+                    wholesaler = "&wholesaler=0";
+                }
+
+                if ($('#retailer').is(':checked')) {
+                    retailer = "&retailer=1";
+                } else {
+                    retailer = "&retailer=0";
+                }
+
+                if ($('#fixed').is(':checked')) {
+                    type_fixed = "&fixed=1";
+                } else {
+                    type_fixed = "&fixed=0";
+                }
+                if ($('#normadic').is(':checked')) {
+                    type_normadic = "&normadic=1";
+                } else {
+                    type_normadic = "&normadic=0";
+                }
+
+                /*if ($('#environmental').is(':checked')) {
+                    environmental = "&environmental=environmental";
+                } else {
+                    environmental = "&environmental=";
+                }
+
+                if ($('#social').is(':checked')) {
+                    social = "&social=social";
+                } else {
+                    social = "&social=";
+                }
+
+                if ($('#economic').is(':checked')) {
+                    economic = "&economic=economic";
+                } else {
+                    economic = "&economic=";
+                }
+
+                if ($('#wellbeing').is(':checked')) {
+                    wellbeing = "&wellbeing=wellbeing";
+                } else {
+                    wellbeing = "&wellbeing=";
+                }*/
+
+
+                var dob = "&age_group="+$('#age_group').val();
+
+                var form = $('#add_user_form').find('input, textarea, select')
+                    .not(':checkbox')
+                    .serialize()
+                //form += environmental + social + wellbeing + economic + service + manufacturer + wholesaler+ retailer + type_fixed + type_normadic + dob;
+
+                form += service + manufacturer + wholesaler+ retailer + type_fixed + type_normadic + dob;
+                console.log(form);
+
+                $.ajax({
+                    type: 'POST',
+                    url: $form.attr('action'),
+                    data: form,
+                    success: function(data)
+                    {
+                        // just to confirm ajax submission
+                        console.log('submitted successfully!');
+                        var received_data = JSON.parse(data);
+                        alert(received_data.message);
+
+                        if(received_data.response == true){
+                            //alert(received_data.message);
                             window.location = "<?=BASE_URL?>";
-						}
-					}
-				  });
-				  
-				  event.preventDefault();
-				},
-				
-				filter: function() {
-						return $(this).is(":visible");
-					}
-					
-			
-			  });
-		});
-		
-	
+                        }
+                    }
+                });
+
+                event.preventDefault();
+            },
+            filter: function() {
+                return $(this).is(":visible");
+            }
+        });
+    });
     </script>
- 
+
   </head>
   <body>
     <div id="header">
@@ -615,7 +602,8 @@ System coordinator</p>
                   </div>
                 </div>
                 <div class="controls controls-row">
-            		<button type="submit" class="btn btn-success  btn-lg btn-block">Register</button>
+                    <button type="submit" class="btn btn-success  btn-lg btn-block">Register</button>
+                    <div id='register_result'></div>
             		</div>
             </fieldset>
             </form>
