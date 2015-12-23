e07f88a0b5a83a583a3c2c2505c9437e87aba7b7
Tom Bloor <t.bloor@shadowcat.co.uk>
2015-12-23 12:59:25 +0000

    Fixed layout of logo, and refactored the header.php file

--- config/header.php
+++ config/header.php
@@ -1,34 +1,29 @@
-<?php
-
-include_once( 'const.php');
-echo '<div id="header_img"><a href="' . BASE_URL . '"><img src="' . BASE_URL . '/images/pear_logo.png" alt="logo" id="logo" /></a></div>
+<?php include_once( 'const.php'); ?>
 <div class="header_container">
 
-      <div class="navbar">
+  <div class="navbar">
 
-        <div class="container">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        
-		 <!-- Be sure to leave the brand out there if you want it shown -->
-    
-	
-          <div class="nav-collapse navbar-responsive-collapse collapse">
-		  <a class="navbar-brand" href="#"></a>
-            <ul class="nav navbar-nav dropdown">
-              <li><a href="' . BASE_URL . '/blog/">Blog</a></li>
-              <li><a href="' . BASE_URL . '/work/">What we working on?</a></li>
-              <li><a href="' . BASE_URL . '/about/get-involved/">Get Involved</a></li>
-			   <li><a href="' . BASE_URL . '/contact/">Contact</a></li>
-			    <li><a title="log-in" href="' . BASE_URL . '/registration/">Sign up</a></li>
-              <li><a title="log-in" href="' . BASE_URL . '/portal/">Log in</a></li>
-            </ul>
+    <div class="container">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
 
-          </div><!-- /.nav-collapse -->
-        </div><!-- /.container -->
-      </div><!-- /.navbar -->
-    </div>';
-?>
+      <div class="nav-collapse navbar-responsive-collapse collapse">
+        <a class="pull-left" href="#">
+          <img src="<? echo BASE_URL; ?>/images/pear_logo.png" alt="logo" id="logo" />
+        </a>
+        <ul class="nav navbar-nav dropdown">
+          <li><a href="<? echo BASE_URL; ?>/blog/">Blog</a></li>
+          <li><a href="<? echo BASE_URL; ?>/work/">What we working on?</a></li>
+          <li><a href="<? echo BASE_URL; ?>/about/get-involved/">Get Involved</a></li>
+          <li><a href="<? echo BASE_URL; ?>/contact/">Contact</a></li>
+          <li><a title="log-in" href="<? echo BASE_URL; ?>/registration/">Sign up</a></li>
+          <li><a title="log-in" href="<? echo BASE_URL; ?>/portal/">Log in</a></li>
+        </ul>
+
+      </div><!-- /.nav-collapse -->
+    </div><!-- /.container -->
+  </div><!-- /.navbar -->
+</div>
