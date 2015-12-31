058cea859f6af267003cb78bee857614970184f5
Tom Bloor <tom.bloor@googlemail.com>
2015-12-31 19:21:55 +0000

    enabled password_hash by upgrading to 5.5.* on server

--- public/test.php
+++ public/test.php
@@ -1,28 +1,7 @@
 <?php
 
-  function encrypt_password( $input, $rounds = 10) {
+  $hashed_pass = password_hash("banana", PASSWORD_BCRYPT);
 
-    $crypt_length = 22;
-    $salt = "";
-    $salt_chars = array_merge(
-      range('A', 'Z'),
-      range('a', 'z'),
-      range(0, 9),
-      array( '.', '/' )
-    );
-
-    for( $i = 0; $i < $crypt_length; $i++ ) {
-      $salt .= $salt_chars[array_rand($salt_chars)];
-    }
-
-    echo $salt;
-    echo "<br>";
-    $crypt_options = sprintf('$2y$%02d$%s', $rounds, $salt);
-    echo $crypt_options;
-    echo "<br>";
-    echo crypt($input, $crypt_options);
-  }
-
-  encrypt_password("");
+  echo $hashed_pass;
 
 ?>
